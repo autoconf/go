c705f03f6174fcffc55de1c529fce8aabb47b822
dell <gmail@ceburakyilmaz.com>
2017-07-16 02:37:45 +0300

    starting to thirdTour

--- tourGo/secondTour/exported-namesGo/exported-names.go
+++ tourGo/secondTour/exported-namesGo/exported-names.go
@@ -6,5 +6,15 @@ import(
 )
 
 func main(){
+/*
+  !!! nesnelerde public / private gibi yazarak ayrım yok !!!
+  bunun yerine public olması için baş harfin büyük olması yeterli
+  yani private nesne küçük harfle
+       public  nesne büyük harfle başlar.
+
+  bu örnekte math paketine ait pi nesnesi public olduğu için küçük harfle
+yazıldığında hata veriyor.
+*/
+//fmt.Println(math.pi)
   fmt.Println(math.Pi)
 }
