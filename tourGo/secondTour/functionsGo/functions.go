371d483af6f8754745847686d435f885f2bde097
dell <gmail@ceburakyilmaz.com>
2017-07-15 06:01:36 +0300

    fix function parameter

--- tourGo/secondTour/functionsGo/functions.go
+++ tourGo/secondTour/functionsGo/functions.go
@@ -3,8 +3,11 @@ package main
 import(
   "fmt"
 )
-//***değişken türü değişken adından sonra geliyor***
-func add(x int, y int) int {
+/*
+***değişken türü değişken adından sonra geliyor***
+**ardışık olarak aynı türden parametre alıyorsa, en sonda bir kere tanımlamak yeterli**
+*/
+func add(x, y int) int {
   return x + y
 }
 
