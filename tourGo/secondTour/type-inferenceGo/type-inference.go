94a7550743e28eee5a909da76b68876fbace050b
dell <gmail@ceburakyilmaz.com>
2017-07-15 08:00:42 +0300

    modified type-inference

--- tourGo/secondTour/type-inferenceGo/type-inference.go
+++ tourGo/secondTour/type-inferenceGo/type-inference.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main(){
-  v := 42
-  fmt.Printf("v is of type %T\n", v)
+  alpha := 42
+  bravo := 3.1415
+  charlie := false
+  delta := "hello"
+
+  fmt.Printf("alpha is of type %T\n", alpha)
+  fmt.Printf("bravo is of type %T\n", bravo)
+  fmt.Printf("charlie is of type %T\n", charlie)
+  fmt.Printf("delta is of type %T\n", delta)
 }
